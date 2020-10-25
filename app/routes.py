de4c4f9d80ee922b4e57317877aa1bb9ad5a8fb1
RJain7098 <rjain@stu.naperville203.org>
2020-10-25 17:20:01 -0500

    Basic search bar
    Created a basic search bar with some field validation

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import app
+from flask import render_template, flash, redirect
+from app.forms import searchForm 
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods = ["GET", "POST"])
+@app.route("/index", methods = ["GET", "POST"])
 def index():
-    return "Hello, World!"
+    form = searchForm() 
+    if form.validate_on_submit():
+        flash("Search initiated with term {}!".format(form.search_term.data))
+        return redirect("/index")
+    return render_template("index.html", form=form)
