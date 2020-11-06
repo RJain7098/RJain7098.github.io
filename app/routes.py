e65c340ee37fdea71e4ba827566f170a53860c06
Rishabh Jain <r.jain.voyager@gmail.com>
2020-11-06 14:53:55 -0600

    updated forms

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,24 @@
 from app import app
-from flask import render_template, flash, redirect, url_for, session
-from app.forms import searchForm, categoryForm
+from flask import render_template, flash, redirect, url_for, session, request
+from app.forms import masterForm
 from app.news import News
 
 @app.route("/", methods = ["GET", "POST"])
 @app.route("/index", methods = ["GET", "POST"])
 def index():
-    form1 = searchForm()
-    form2 = categoryForm() 
-    for choice in form2.category_form.choices:
-        print(choice)
-    if form1.validate_on_submit() and form2.validate_on_submit():
-        session["q"] = form1.search_term.data
-        session["category"] = form2.category_form.data
+    form = masterForm()
+    if request.method == "POST" and form.validate():
+        session["q"] = form.search_term.data
+        session["category"] = form.category_form.data
         return redirect(url_for("results"))
     else:
-        print(form2.errors)
-    return render_template("index.html", form1=form1, form2=form2)
+        print(form.errors)
+    return render_template("index.html", form=form)
 
 @app.route("/results")  
 def results():
     q = session.get("q", None)
-    category = session.get("category", None)
+    category = session.get("category", None).lower()
     print(category)
     searcher = News()
     results = searcher.search(q, category)
