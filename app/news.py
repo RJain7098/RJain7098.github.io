7be63ceaaffd2e725098992b5f14aea7c12798d5
Rishabh Jain <r.jain.voyager@gmail.com>
2020-11-06 21:39:13 -0600

    updated results
    missed one thing

--- app/news.py
+++ app/news.py
@@ -11,7 +11,4 @@ class News():
         top_headlines = newsapi.get_top_headlines(q = q, category = category)
         return top_headlines
 
-test = News()
-print(test.search("google", "entertainment"))
-
 
